b254e245b6fe3b28e8ad0f3a826d7f8f6f2f2423
Rabanti <Rabanti13010@outlook.com>
2025-10-29 19:42:18 +0100

    Réalisation page itinéraires+ Debut détail

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,9 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/covoiturages", "Covoiturages", "/pages/covoiturages.html"),];
+    new Route("/covoiturages", "Covoiturages", "/pages/covoiturages.html"),
+    new Route("/itineraires", "Itineraires", "/pages/itineraires.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Ecoride";
