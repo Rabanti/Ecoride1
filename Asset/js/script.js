c9a90e8d9f80f94cb80cf639545874930dd469eb
Rabanti <Rabanti13010@outlook.com>
2025-11-01 20:06:43 +0100

    avancement commit

---
+++ Asset/js/script.js
@@ -0,0 +1,12 @@
+// les conditions if : else if; else
+//Grâce à ca le programme va pouvoir éxécuter différentes actions en fonctions de certaines conditions c'est un concèpte clé pour créer des appli dynamique qui réagissent aux actions des utilisateurs
+// déclare une variable age et lui affecter une valeur
+// si l'age est supérieur ou égale à 18 afficher vous êtes majeur sinon vous êtes mineur
+
+
+
+
+
+    let villeDepart = parseFloat(prompt("Entrez une ville de départ :"));
+    let dateDepart = parseFloat(prompt("Entrez une date de départ :"));
+
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,11 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/covoiturages", "Covoiturages", "/pages/covoiturages.html"),
-    new Route("/itineraires", "Itineraires", "/pages/itineraires.html"),
+    new Route("/itineraires", "itineraires", "/pages/itineraires.html"),
+    new Route("/leaDetail", "lea Detail", "/pages/Detail/leaDetail.html"),
+    new Route("/alexDetail", "alex Detail", "/pages/Detail/alexDetail.html"),
+    new Route("/jeffDetail", "jeff Detail", "/pages/Detail/jeffDetail.html"),
+    new Route("/creationCompte", "création de Compte", "/pages/creationCompte.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
